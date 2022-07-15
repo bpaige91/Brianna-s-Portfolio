50600702826272fbafd0c1d81805fa8b0d772105
Brianna Jackson <Briannajackson@quickenloans.com>
2022-07-14 23:18:39 -0400

    adding project to portfolio

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
-const nacLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 })
 
 navLinks.forEach(link => {
-    link.addEventListener('click', ())=> {
+     link.addEventListener('click', () ) => {
         document.body.classList.remove('nav-open');
     }
 })
